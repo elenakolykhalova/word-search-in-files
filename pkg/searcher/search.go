740c5159eca86f49207fb26cde85674cef3ee936
elenakolykhalova <94307455+elenakolykhalova@users.noreply.github.com>
2024-05-29 13:59:53 +0300

    Update search.go
    add func search

--- pkg/searcher/search.go
+++ pkg/searcher/search.go
@@ -2,6 +2,11 @@ package searcher
 
 import (
 	"io/fs"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"sync"
+	"word-search-in-files/pkg/internal/dir"
 )
 
 type Searcher struct {
@@ -9,6 +14,64 @@ type Searcher struct {
 }
 
 func (s *Searcher) Search(word string) (files []string, err error) {
-	//dir.FilesFS(s.FS, ".")?
-	return
+
+	// Слайс списка всех файлов в текущей директории
+	allFiles, err := dir.FilesFS(s.FS, ".")
+	if err != nil {
+		return nil, err
+	}
+
+	// Регулярное выражение для точного поиска слова, можно было бы использовать strings.Contains,
+	//но тогда выходили бы все слова, которые так же включают частичное сопадение искомого слова
+	regexPattern := `\b` + regexp.QuoteMeta(word) + `\b`
+	re, err := regexp.Compile(regexPattern)
+	if err != nil {
+		return nil, err
+	}
+
+	results := make(chan string) // Канал для передачи результатов
+	errs := make(chan error)     // Канал для передачи ошибок
+	var wg sync.WaitGroup        // Для ожидания завершения всех горутин
+
+	// Запуск поиска в каждом файле с помощью горутин
+	for _, path := range allFiles {
+		wg.Add(1)
+		go func(path string) {
+			defer wg.Done()
+			// Читаем содержимое файла
+			content, err := fs.ReadFile(s.FS, path)
+			if err != nil {
+				errs <- err
+				return
+			}
+
+			// Проверка содержимого файла
+			if re.Match(content) {
+				// Убираем из имени файла расширение
+				filename := strings.TrimSuffix(filepath.Base(path), filepath.Ext(path))
+				results <- filename
+			}
+		}(path)
+	}
+
+	// Ожидаем завершение всех горутин и закрываем каналы
+	go func() {
+		wg.Wait()
+		close(results)
+		close(errs)
+	}()
+
+	// Сбор результатов
+	for result := range results {
+		files = append(files, result)
+	}
+
+	// Проверка ошибок
+	for err := range errs {
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return files, nil
 }
